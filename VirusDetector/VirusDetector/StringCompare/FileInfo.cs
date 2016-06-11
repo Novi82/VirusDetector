c62141693783106f17b74780112c99a48d338348
oritoka <oritoka@gmail.com>
2016-06-12 02:10:52 +0700

    Str Compare

---
+++ VirusDetector/VirusDetector/StringCompare/FileInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirusDetector.StringCompare
+{
+    class FileInfo
+    {
+        String _fileName;
+
+        public String FileName
+        {
+            get { return _fileName; }
+            set { _fileName = value; }
+        }
+        int _mark;
+
+        public int Mark
+        {
+            get { return _mark; }
+            set { _mark = value; }
+        }
+
+        public FileInfo()
+        {
+            _fileName = "";
+            _mark = Utils.Utils.BENIGN_MARK;
+        }
+
+        public FileInfo(String fileName_, int mark_)
+        {
+            _fileName = fileName_;
+            _mark = mark_;
+        }
+    }
+}
